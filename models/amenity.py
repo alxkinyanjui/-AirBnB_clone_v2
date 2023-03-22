6bf2868a0d013de530fd81f67a0228eeb47eadf2
alxkinyanjui <alexgkinyanjui@gmail.com>
2023-03-22 03:03:20 -0700

    All Tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from os import getenv
+from models import storage_type
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
-    """Representation of Amenity """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'amenities'
+class Amenity(BaseModel, Base):
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="amenities"
-        )
     else:
         name = ""
